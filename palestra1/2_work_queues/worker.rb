fbb029c0a9a8754226d9e6bbf8b0ea6f38d23e91
igor-paiva <igorbatistapaiva@outlook.com>
2022-07-09 07:49:18 -0300

    Adds RabbitMQ patterns examples

---
+++ palestra1/2_work_queues/worker.rb
@@ -0,0 +1,23 @@
+require 'bunny'
+
+connection = Bunny.new(automatically_recover: false)
+connection.start
+
+channel = connection.create_channel
+queue = channel.queue('task_queue', durable: true)
+
+# Don't give more than one message to a worker at a time
+channel.prefetch(1)
+puts ' [*] Waiting for messages. To exit press CTRL+C'
+
+begin
+  queue.subscribe(manual_ack: true, block: true) do |delivery_info, _properties, body|
+    puts " [x] Received '#{body}'"
+    # imitate some work
+    sleep body.count('.').to_i
+    puts ' [x] Done'
+    channel.ack(delivery_info.delivery_tag)
+  end
+rescue Interrupt => _
+  connection.close
+end
