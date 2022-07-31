c02aa8d4597479b37a4a9b563440cff8b8ca1342
igor-paiva <igorbatistapaiva@outlook.com>
2022-07-31 17:23:12 -0300

    Adds map reduce word count example

---
+++ lab3/word_count/generate_file.rb
@@ -0,0 +1,30 @@
+require 'faker'
+
+methods = %i[famous_last_words most_interesting_man_in_the_world jack_handey matz robin singular_siegler yoda]
+
+megabytes_to_write = ARGV.size > 0 ?  ARGV[0].to_i : 250
+output_filename = ARGV.size > 1 ?  ARGV[1] : 'input.txt'
+
+bytes_to_write = megabytes_to_write << 20
+
+bytes_written = 0
+
+srand(Time.now.to_i)
+
+File.open(output_filename, 'w') do |file|
+  loop do
+    break if bytes_written >= bytes_to_write
+
+    methods.each do |method_name|
+      quote = " #{Faker::Quote.send(method_name)}"
+
+      quote.gsub!(/[^a-zA-Z\s]/, ' ')
+
+      bytes_written += file.write(quote)
+    end
+
+    file.write("\n") if rand(0..9) % 2
+  end
+end
+
+puts "#{bytes_written} written"
